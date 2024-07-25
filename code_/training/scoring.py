e6b5dca7ae564c6126a5e6d924b95353eb206bbb
Sina-Dntp <157230458+Sina-dntp1379@users.noreply.github.com>
2024-07-25 01:18:23 -0400

    Update scoring.py

--- code_/training/scoring.py
+++ code_/training/scoring.py
@@ -65,46 +65,6 @@ rmse_scorer = make_scorer(rmse_score, greater_is_better=False)
 mae_scorer = make_scorer(mean_absolute_error, greater_is_better=False)
 
 
-# def process_scores(
-#     scores: dict[int, dict[str, float]]
-# ) -> dict[Union[int, str], dict[str, float]]:
-#     # TODO: Handling multi-output
-#     # sample_size: int = 7 * 5  # N seeds * N folds
-#     sample_size: int = 7  # N seeds since we're essentially doing a paired t-test
-
-#     avg_r = round(np.mean([seed["test_r"] for seed in scores.values()]), 2)
-#     stdev_r = round(np.std([seed["test_r"] for seed in scores.values()]), 2)
-#     # stderr_r = round(stdev_r / np.sqrt(sample_size), 2)
-#     avg_r2 = round(np.mean([seed["test_r2"] for seed in scores.values()]), 2)
-#     stdev_r2 = round(np.std([seed["test_r2"] for seed in scores.values()]), 2)
-#     # stderr_r2 = round(stdev_r2 / np.sqrt(sample_size), 2)
-#     print("Average scores:\t", f"r: {avg_r}±{stdev_r}\t", f"r2: {avg_r2}±{stdev_r2}")
-
-#     first_key = list(scores.keys())[0]
-#     score_types: list[str] = [
-#         key for key in scores[first_key].keys() if key.startswith("test_")
-#     ]
-#     # score_types: list[str] = [score.replace("test_", "") for score in score_types]
-#     avgs: list[float] = [
-#         np.mean([seed[score] for seed in scores.values()]) for score in score_types
-#     ]
-#     stdevs: list[float] = [
-#         np.std([seed[score] for seed in scores.values()]) for score in score_types
-#     ]
-#     std_errs: list[float] = [stdev / np.sqrt(sample_size) for stdev in stdevs]
-
-#     # score_types: list[str] = ["r", "r2", "rmse", "mae"]
-#     # avgs: list[float] = [np.mean([seed[f"test_{score}"] for seed in scores.values()]) for score in score_types]
-#     # stdevs: list[float] = [np.std([seed[f"test_{score}"] for seed in scores.values()]) for score in score_types]
-#     # std_errs: list[float] = [stdev / np.sqrt(sample_size) for stdev in stdevs]
-#     score_types: list[str] = [score.replace("test_", "") for score in score_types]
-#     for score, avg, stdev, stderr in zip(score_types, avgs, stdevs, std_errs):
-#         scores[f"{score}_avg"] = abs(avg) if score in ["rmse", "mae"] else avg
-#         scores[f"{score}_stdev"] = stdev
-#         scores[f"{score}_stderr"] = stderr
-
-#     return scores
-
 def process_scores(
     scores: dict[int, dict[str, float]]
     ) -> dict[Union[int, str], dict[str, float]]:
