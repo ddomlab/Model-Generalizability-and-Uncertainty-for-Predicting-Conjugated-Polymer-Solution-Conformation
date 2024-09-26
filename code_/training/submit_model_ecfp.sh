8104db8b0180a5103282b6a7973a585a0e7acc73
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-09-26 01:14:02 -0400

    submit job of ecfp uploaded

--- code_/training/submit_model_ecfp.sh
+++ code_/training/submit_model_ecfp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results
 
-radii = (3 4 5 6)
-vectors = ('count' 'binary')
+radii=(3 4 5 6)  # Fixed array declaration
+vectors=('count' 'binary')  # Fixed array declaration
 
 for radius in "${radii[@]}"; do
   for vector in "${vectors[@]}"; do
@@ -11,7 +11,7 @@ for radius in "${radii[@]}"; do
 #BSUB -W 35:05
 #BSUB -R span[ptile=4]
 #BSUB -R "rusage[mem=32GB]"
-#BSUB -J ${output_dir}/finger_radius${radius}_vector${vector}
+#BSUB -J "finger_radius${radius}_vector${vector}"  # Simplified job name
 #BSUB -o ${output_dir}/ecfp_run_radius${radius}_vector${vector}.out
 #BSUB -e ${output_dir}/ecfp_err_radius${radius}_vector${vector}.out
 
