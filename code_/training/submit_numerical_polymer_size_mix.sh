82a37a95e43fed2c70b585a427f2cf59b0a319dc
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-10-03 11:46:47 -0400

    submit updated for mw and mn

--- code_/training/submit_numerical_polymer_size_mix.sh
+++ code_/training/submit_numerical_polymer_size_mix.sh
@@ -24,7 +24,7 @@ conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python train_structure_numeric.py --target_features "${target}" \
                                   --regressor_type "${model}" \
                                   --numerical_feats 'Mn (g/mol)' 'PDI' 'Mw (g/mol)' \
-                                  --columns_to_impute 'Mn (g/mol)' 'PDI' 'Mw (g/mol)' \
+                                  --columns_to_impute  'PDI' \
                                   --special_impute 'Mw (g/mol)'\
                                   --imputer mean
 
