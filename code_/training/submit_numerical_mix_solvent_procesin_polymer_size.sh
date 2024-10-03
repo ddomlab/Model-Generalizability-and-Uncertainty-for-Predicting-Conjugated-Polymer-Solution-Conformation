d5f901bfd0c5b318ac3e03a688170e696a77b260
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-10-03 11:13:56 -0400

    submit polymer updated

---
+++ code_/training/submit_numerical_mix_solvent_procesin_polymer_size.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results
+
+# Correctly define models and numerical features
+target_to_asses=("Lp (nm)" "Rg1 (nm)")
+models_to_run=("RF" "MLR" "DT")
+
+
+for target in "${target_to_asses[@]}"; do
+    for model in "${models_to_run[@]}"; do
+        bsub <<EOT
+
+#BSUB -n 8
+#BSUB -W 30:01
+#BSUB -R span[ptile=4]
+##BSUB -x
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -J numerical_${model}_with_feats_on_${target}
+#BSUB -o ${output_dir}/numerical_${model}_with_feats_on_${target}.out
+#BSUB -e ${output_dir}/numerical_${model}_with_feats_on_${target}.err
+
+source ~/.bashrc
+conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
+python train_structure_numeric.py --target_features "${target}" \
+                                  --regressor_type "${model}" \
+                                  --numerical_feats 'Mn (g/mol)' 'PDI' 'Mw (g/mol)' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "solvent dP" "solvent dD" "solvent dH" \
+                                  --columns_to_impute 'Mn (g/mol)' 'PDI' 'Mw (g/mol)' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' \
+                                  --special_impute 'Mw (g/mol)'\
+                                  --imputer mean
+
+
+conda deactivate
+
+EOT
+        
+    done
+done
---
+++ code_/training/submit_numerical_polymer_size.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_out
+
+# Correctly define models and numerical features
+target_to_asses=("Lp (nm)" "Rg1 (nm)")
+models_to_run=("RF" "MLR" "DT")
+numerical_feats=(
+  "PDI"
+  "Mw (g/mol)"
+)
+
+combined_feats="${numerical_feats[0]} ${numerical_feats[1]} ${numerical_feats[2]}"
+
+# Append the combined features to the array
+numerical_feats+=("$combined_feats")
+
+for target in "${target_to_asses[@]}"; do
+        for model in "${models_to_run[@]}"; do
+            for feats in "${numerical_feats[@]}"; do
+                bsub <<EOT
+
+#BSUB -n 8
+#BSUB -W 30:01
+#BSUB -R span[ptile=4]
+##BSUB -x
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -J numerical_${model}_with_${feats}_on_${target}
+#BSUB -o ${output_dir}/numerical_${model}_with_${feats}.out
+#BSUB -e ${output_dir}/numerical_${model}_with_${feats}.err
+
+source ~/.bashrc
+conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
+python train_structure_numeric.py --target_features "${target}" \
+                                  --regressor_type "${model}" \
+                                  --numerical_feats "${feats}" \
+                                  --columns_to_impute ${feats}" \
+                                  --imputer mean
+conda deactivate
+
+EOT
+        done
+    done
+done
