bb03c564557d9ef0a055111b5c19f6f6ac71f2e9
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-10-04 10:52:42 -0400

    submit splitted to models

--- code_/training/submit_structural_numerical_maccs_mordred.sh
+++ code_/training/submit_structural_numerical_maccs_mordred.sh
@@ -3,7 +3,7 @@ output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset
 # Define arrays for regressor types, targets, and models
 regressors=("RF" "MLR" "XGBR")
 targets=("Rg1 (nm)" "Lp (nm)")
-models=("ecfp" "maccs" "mordred")
+models=("maccs" "mordred")
 
 # Define the output directory (make sure to create it beforehand)
 output_dir="/path/to/output"  # Change this to the appropriate directory
@@ -12,29 +12,8 @@ output_dir="/path/to/output"  # Change this to the appropriate directory
 for regressor in "${regressors[@]}"; do
   for target in "${targets[@]}"; do
     for model in "${models[@]}"; do
-      # For ECFP model, you can vary radius and vector
-      if [ "$model" == "ecfp" ]; then
-        for radius in 3 4 5 6; do
-          for vector in "count" "binary"; do
-            # Submitting the job using bsub
-            bsub <<EOT
-#BSUB -n 8
-#BSUB -W 35:05
-#BSUB -R span[ptile=4]
-#BSUB -R "rusage[mem=32GB]"
-#BSUB -J "ecfp_radius${radius}_vector${vector}"  # Job name
-#BSUB -o ${output_dir}/ecfp_run_radius${radius}_vector${vector}.out
-#BSUB -e ${output_dir}/ecfp_err_radius${radius}_vector${vector}.out
-
-source ~/.bashrc
-conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
-python train_structure_numerical.py $model --regressor_type $regressor --radius $radius --vector $vector --target "$target"
-EOT
-          done
-        done
-      else
-        # Submitting the job for MACCS or Mordred models
-        bsub <<EOT
+      # Submitting the job using bsub
+      bsub <<EOT
 #BSUB -n 8
 #BSUB -W 35:05
 #BSUB -R span[ptile=4]
@@ -47,7 +26,6 @@ source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python train_structure_numerical.py $model --regressor_type $regressor --target "$target"
 EOT
-      fi
     done
   done
 done
