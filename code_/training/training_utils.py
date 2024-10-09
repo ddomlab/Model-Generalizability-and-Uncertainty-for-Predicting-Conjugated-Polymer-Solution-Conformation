75622a20f8a770a1da20b6587d4719e343e2fb6b
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-10-09 15:23:45 -0400

    training updated with TEST

--- code_/training/training_utils.py
+++ code_/training/training_utils.py
@@ -48,7 +48,7 @@ from sklearn.metrics import (
 HERE: Path = Path(__file__).resolve().parent
 
 # os_type: str = platform.system().lower()
-TEST=True
+TEST=False
 
 # Seeds for generating random states
 if TEST==False:
