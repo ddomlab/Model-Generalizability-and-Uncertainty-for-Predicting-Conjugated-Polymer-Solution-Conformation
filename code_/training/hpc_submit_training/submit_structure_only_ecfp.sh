16bc9c559d9cb520809b3f396ea9d17604fbeea1
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-10-21 19:04:01 -0400

    updated submit for ecfp only

--- code_/training/hpc_submit_training/submit_structure_only_ecfp.sh
+++ code_/training/hpc_submit_training/submit_structure_only_ecfp.sh
@@ -3,9 +3,9 @@ output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset
 # Define arrays for regressor types, targets, and models
 regressors=("NGB")
 targets=("Rg1 (nm)")
-radii=(3) 
-poly_representations=('RRU Dimer')
-vectors=("binary")
+radii=(3 4 5 6) 
+poly_representations=('Monomer' 'Dimer' 'Trimer' 'RRU Monomer' 'RRU Dimer' 'RRU Trimer')
+vectors=("binary" "count")
 
 # Loop through each combination of regressor, target, and model
 for regressor in "${regressors[@]}"; do
@@ -25,7 +25,7 @@ for regressor in "${regressors[@]}"; do
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
-python train_structure_only.py ecfp --regressor_type $regressor --radius $radius --vector $vector --target "$target" --oligo_type "$oligo_rep"
+python ../train_structure_only.py ecfp --regressor_type $regressor --radius $radius --vector $vector --target "$target" --oligo_type "$oligo_rep"
 EOT
         done
       done
