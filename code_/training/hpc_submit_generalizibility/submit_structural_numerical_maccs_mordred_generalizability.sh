94af37c18d30a890214b20357d33024670aafa73
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-10-21 18:59:42 -0400

    uploaded submit mordred numerical for generalizability

--- code_/training/hpc_submit_generalizibility/submit_structural_numerical_maccs_mordred_generalizability.sh
+++ code_/training/hpc_submit_generalizibility/submit_structural_numerical_maccs_mordred_generalizability.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results
 # Define arrays for regressor types, targets, and models
-regressors=("NGB")
+regressors=("NGB" "XGBR")
 targets=("Rg1 (nm)")
 models=("mordred")
-poly_representations=("RRU Monomer")
+poly_representations=('Monomer' 'Dimer' 'Trimer' 'RRU Monomer' 'RRU Dimer' 'RRU Trimer')
 
 
 
@@ -17,16 +17,17 @@ for regressor in "${regressors[@]}"; do
       
       bsub <<EOT
 #BSUB -n 8
-#BSUB -W 5:01
+#BSUB -W 10:01
 #BSUB -R span[ptile=4]
 #BSUB -R "rusage[mem=32GB]"
-#BSUB -J "${oligo_rep}_${target}_${model}_${regressor}_test_structure_only"  
-#BSUB -o test.out
-#BSUB -e test.err
+#BSUB -J "structure_numerical_mordred_NGB_generalizibility"  
+#BSUB -o ${output_dir}/structure_numerical_mordred_NGB_generalizibili_${regressor}.out
+#BSUB -e ${output_dir}/structure_numerical_mordred_NGB_generalizibili_${regressor}.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
-python train_structure_only.py $model --regressor_type $regressor --target "$target" --oligo_type "$oligo_rep"
+python ../test_generalizability_structure_numerical.py $model --regressor_type $regressor --target "$target" --oligo_type "$oligo_rep"
+
 EOT
       done
     done
--- code_/training/hpc_submit_generalizibility/submit_structural_polysize_maccs_mordred_generalizibility.sh
+++ code_/training/hpc_submit_generalizibility/submit_structural_polysize_maccs_mordred_generalizibility.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results
 # Define arrays for regressor types, targets, and models
-regressors=("NGB")
+regressors=("NGB" "XGBR")
 targets=("Rg1 (nm)")
 models=("mordred")
 poly_representations=('Monomer' 'Dimer' 'Trimer' 'RRU Monomer' 'RRU Dimer' 'RRU Trimer')
-
-
 
 
 # Loop through each combination of regressor, target, and model
@@ -14,19 +12,19 @@ for regressor in "${regressors[@]}"; do
   for target in "${targets[@]}"; do
     for model in "${models[@]}"; do
       for oligo_rep in "${poly_representations[@]}"; do
-      
       bsub <<EOT
 #BSUB -n 8
-#BSUB -W 30:01
+#BSUB -W 10:00
 #BSUB -R span[ptile=4]
 #BSUB -R "rusage[mem=32GB]"
-#BSUB -J "structure_only_mordred_NGB_generalizibility"  
-#BSUB -o "${output_dir}/structure_only_mordred_NGB_generalizibility.out"
-#BSUB -e "${output_dir}/test_structure_only_mordred_NGB_generalizibility.err"
+#BSUB -J "${model}_${regressor}_structure_only" 
+#BSUB -o ${output_dir}/model_${model}_${target}_${regressor}_.out
+#BSUB -e ${output_dir}/model_${model}_${target}_${regressor}_.err
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
-python ../test_generalizability_structure_only.py $model --regressor_type $regressor --target "$target" --oligo_type "$oligo_rep"
+python train_structure_numerical.py $model --regressor_type $regressor --target "$target" --oligo_type $oligo_rep
+#! not complete yet!
 
 EOT
       done
