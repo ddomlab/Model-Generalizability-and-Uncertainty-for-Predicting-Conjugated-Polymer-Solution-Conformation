7bda35213ac22a8b59e8bb0b4acccb2594b9631d
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-11-19 10:35:19 -0500

    updated submit for all hsp single

--- code_/training/hpc_submit_training_Rh/submit_numerical_(concentration+temperature)_general.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_(concentration+temperature)_general.sh
@@ -4,10 +4,7 @@ output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset
 # Correctly define models and numerical features
 target_to_asses=("Rh (IW avg log)")
 models_to_run=("RF" "MLR" "DT")
-numerical_feats=(
-  "Concentration (mg/ml)"
-  "Temperature SANS/SLS/DLS/SEC (K)"
-)
+numerical_feats=("polymer dP" "polymer dD" "polymer dH" "solvent dP" "solvent dD" "solvent dH" "Ra")
 
 for target in "${target_to_asses[@]}"; do
         for model in "${models_to_run[@]}"; do
@@ -19,9 +16,9 @@ for target in "${target_to_asses[@]}"; do
 #BSUB -R span[ptile=4]
 ##BSUB -x
 #BSUB -R "rusage[mem=32GB]"
-#BSUB -J numerical_${model}_with_${feats}_on_${target}
-#BSUB -o ${output_dir}/numerical_${model}_with_${feats}.out
-#BSUB -e ${output_dir}/numerical_${model}_with_${feats}.err
+#BSUB -J "numerical_${model}_with_${feats}_on_${target}"
+#BSUB -o "${output_dir}/numerical_${model}_with_${feats}_on_${target}.out"
+#BSUB -e "${output_dir}/numerical_${model}_with_${feats}_on_${target}.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
@@ -29,8 +26,6 @@ conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                   --regressor_type "${model}" \
                                   --numerical_feats "${feats}" \
-                                  --columns_to_impute "${feats}" \
-                                  --imputer mean
 
 conda deactivate
 
