33b4dab2849c670d2f71a41775b0860b6edef4b8
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-11-19 10:46:35 -0500

    submit for solvent hsp

--- code_/training/hpc_submit_training_Rh/submit_numerical_just_HSP.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_just_HSP.sh
@@ -14,15 +14,15 @@ for target in "${target_to_asses[@]}"; do
 #BSUB -W 30:01
 #BSUB -R span[ptile=4]
 #BSUB -R "rusage[mem=32GB]"
-#BSUB -J "poly_HSP_with_${model}_on_${target}_no_Ra_solvent"
-#BSUB -o "${output_dir}/poly_HSP_with_${model}_on_${target}_no_Ra_solvent.out"
-#BSUB -e "${output_dir}/poly_HSP_with_${model}_on_${target}_no_Ra_solvent.err"
+#BSUB -J "poly_HSP_with_${model}_on_${target}_no_Ra_poly"
+#BSUB -o "${output_dir}/solvent_HSP_with_${model}_on_${target}_no_Ra_poly.out"
+#BSUB -e "${output_dir}/solvent_HSP_with_${model}_on_${target}_no_Ra_poly.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                   --regressor_type "${model}" \
-                                  --numerical_feats "polymer dP" "polymer dD" "polymer dH"
+                                  --numerical_feats "solvent dP" "solvent dD" "solvent dH"
                                   
 conda deactivate
 
@@ -31,4 +31,3 @@ EOT
     done
 done
 
-# "solvent dP" "solvent dD" "solvent dH"
