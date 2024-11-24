543d82faeaddff394d634f4de5c019bffe725379
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-11-24 10:27:09 -0500

    updated for |(d polymer-d solvent)|

--- code_/training/hpc_submit_training_Rh/submit_numerical_(concentration+temperature)_general.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_(concentration+temperature)_general.sh
@@ -2,13 +2,13 @@
 output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_out
 
 # Correctly define models and numerical features
-target_to_asses=("Rh (IW avg log)")
+target_to_asses=("Rh (IW avg log)" "Rg1 (nm)")
 models_to_run=("RF" "MLR" "DT")
-numerical_feats=("polymer dP" "polymer dD" "polymer dH" "solvent dP" "solvent dD" "solvent dH" "Ra")
+numerical_feat_list=("|solvent dD - polymer dD|" "|solvent dP - polymer dP|" "|solvent dH - polymer dH|")
 
 for target in "${target_to_asses[@]}"; do
         for model in "${models_to_run[@]}"; do
-            for feats in "${numerical_feats[@]}"; do
+            for feats in "${numerical_feat_list[@]}"; do
                 
                 bsub <<EOT
 
@@ -17,9 +17,9 @@ for target in "${target_to_asses[@]}"; do
 #BSUB -R span[ptile=4]
 ##BSUB -x
 #BSUB -R "rusage[mem=32GB]"
-#BSUB -J "numerical_${model}_with_${feats}_on_${target}"
-#BSUB -o "${output_dir}/numerical_${model}_with_${feats}_on_${target}.out"
-#BSUB -e "${output_dir}/numerical_${model}_with_${feats}_on_${target}.err"
+#BSUB -J "${feats}_${model}_with_${feats}_on_${target}"
+#BSUB -o "${output_dir}/${feats}_with_${model}_with_${feats}_on_${target}.out"
+#BSUB -e "${output_dir}/${feats}_with_${model}_with_${feats}_on_${target}.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
