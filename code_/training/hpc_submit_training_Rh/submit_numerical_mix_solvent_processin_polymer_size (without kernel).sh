156f10cb18e38a1f29630b1e2e03f6971d20f9fa
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-12-20 16:53:06 -0500

    updated results

--- code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
@@ -4,7 +4,7 @@ output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset
 # Correctly define models and numerical features
 target_to_asses=("Rg1 (nm)")
 models_to_run=("RF" "MLR" "DT")
-scaler_types=('Standard' 'Robust Scaler')
+scaler_types=('Robust Scaler')
 
 for target in "${target_to_asses[@]}"; do
     for model in "${models_to_run[@]}"; do
@@ -16,13 +16,14 @@ for target in "${target_to_asses[@]}"; do
 #BSUB -R span[ptile=2]
 #BSUB -R "rusage[mem=16GB]"
 #BSUB -J "numerical_${model}_polymer_size_feats_on_${target}_all_num"
-#BSUB -o "${output_dir}/numerical_${model}_${scaler}_${target}_final1.out"
-#BSUB -e "${output_dir}/numerical_${model}_${scaler}_${target}_final1.err"
+#BSUB -o "${output_dir}/numerical_${model}_${scaler}_${target}_final.out"
+#BSUB -e "${output_dir}/numerical_${model}_${scaler}_${target}_final.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                     --regressor_type "${model}" \
+                                    --transform_type "${scaler}" \
                                     --numerical_feats 'Mn (g/mol)' 'PDI' 'Mw (g/mol)' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" "solvent dP" "solvent dD" "solvent dH" \
                                     --columns_to_impute "PDI" "Temperature SANS/SLS/DLS/SEC (K)" "Concentration (mg/ml)" \
                                     --special_impute 'Mw (g/mol)' \
