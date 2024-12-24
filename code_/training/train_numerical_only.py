b96cc1133150f66a02f75283cbce954b821d1dd7
Sina-dntp1379 <sdehgha2@ncsu.edu>
2024-12-24 12:17:04 -0500

    updated

--- code_/training/train_numerical_only.py
+++ code_/training/train_numerical_only.py
@@ -94,7 +94,7 @@ def parse_arguments():
     parser.add_argument(
         '--regressor_type', 
         type=str, 
-        choices=['RF', 'DT', 'MLR', 'SVR', 'XGBR','KNN', 'GPR', 'NGB'], 
+        choices=['RF', 'DT', 'MLR', 'SVR', 'XGBR','KNN', 'GPR', 'NGB', 'sklearn-GPR'], 
         required=True, 
         help="Regressor type required"
     )
