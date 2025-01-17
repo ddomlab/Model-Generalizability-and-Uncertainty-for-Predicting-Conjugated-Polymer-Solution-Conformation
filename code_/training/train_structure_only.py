041edcd4d1a848f486b527105d3ee44965a634f6
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-01-17 14:31:05 -0500

    updated

--- code_/training/train_structure_only.py
+++ code_/training/train_structure_only.py
@@ -15,7 +15,7 @@ HERE: Path = Path(__file__).resolve().parent
 DATASETS: Path = HERE.parent.parent / "datasets"
 RESULTS = Path = HERE.parent.parent / "results"
 
-training_df_dir: Path = DATASETS/ "training_dataset"/ "training_dataset"/"dataset_wo_block_cp_(fp-hsp)_added_additive_dropped_polyHSP_dropped_peaks_appended_multimodal (40-1000 nm)_added.pkl"
+training_df_dir: Path = DATASETS/ "training_dataset"/ "dataset_wo_block_cp_(fp-hsp)_added_additive_dropped_polyHSP_dropped_peaks_appended_multimodal (40-1000 nm)_added.pkl"
 # oligo_dir: Path = DATASETS/ "raw"/"pu_columns_used.json"
 
 # oligomer_list =open_json(oligo_dir)
