94b74985836e83c49ea1333d6213f6a1db315581
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-01-20 00:03:13 -0500

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
@@ -15,7 +15,7 @@ for target in "${target_to_asses[@]}"; do
 #BSUB -W 25:01
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
-#BSUB -J "numerical_${model}_polymer_size_feats_on_${target}_all_num_20250118"
+#BSUB -J "numerical_${model}_polymer_size_feats_on_${target}_all_num_20250119"
 #BSUB -o "${output_dir}/numerical_${model}_${scaler}_${target}_20250118.out"
 #BSUB -e "${output_dir}/numerical_${model}_${scaler}_${target}_20250118.err"
 
