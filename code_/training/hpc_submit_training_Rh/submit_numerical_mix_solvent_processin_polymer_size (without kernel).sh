b4051abad4df279a6efcc5c476bd73736e3fb854
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-01-20 11:54:19 -0500

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
@@ -15,9 +15,9 @@ for target in "${target_to_asses[@]}"; do
 #BSUB -W 25:01
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
-#BSUB -J "numerical_${model}_polymer_size_feats_on_${target}_all_num_20250119"
-#BSUB -o "${output_dir}/numerical_${model}_${scaler}_${target}_20250119.out"
-#BSUB -e "${output_dir}/numerical_${model}_${scaler}_${target}_20250119.err"
+#BSUB -J "numerical_${model}_polymer_size_feats_on_${target}_all_num_20250120"
+#BSUB -o "${output_dir}/numerical_${model}_${scaler}_${target}_20250120.out"
+#BSUB -e "${output_dir}/numerical_${model}_${scaler}_${target}_20250120.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
