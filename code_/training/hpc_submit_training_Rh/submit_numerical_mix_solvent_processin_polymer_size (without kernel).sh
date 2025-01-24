562ca1bf9b876d31d7780026f4b928249bcb0a40
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-01-24 10:16:06 -0500

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_mix_solvent_processin_polymer_size (without kernel).sh
@@ -24,7 +24,9 @@ conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                     --regressor_type "${model}" \
                                     --transform_type "${scaler}" \
-                                    --numerical_feats "polymer dP" "polymer dD" "polymer dH" \
+                                    --numerical_feats  'PDI' 'Mn (g/mol)' 'Mw (g/mol)' 'Concentration (mg/ml)' "Temperature SANS/SLS/DLS/SEC (K)" \
+                                    --columns_to_impute "PDI" "Temperature SANS/SLS/DLS/SEC (K)" "Concentration (mg/ml)" \
+                                    --imputer mean
 
 
 conda deactivate
@@ -55,4 +57,3 @@ done
     #                                 --imputer mean
 
     #--special_impute 'Mw (g/mol)' \
-                                        # --columns_to_impute "Temperature SANS/SLS/DLS/SEC (K)" "Concentration (mg/ml)" \
