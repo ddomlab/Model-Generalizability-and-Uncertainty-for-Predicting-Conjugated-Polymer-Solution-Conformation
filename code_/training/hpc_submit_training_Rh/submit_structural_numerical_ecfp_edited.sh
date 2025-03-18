1641b05b000460117acbf9e41bd18566c9841baa
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-03-17 20:47:50 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_structural_numerical_ecfp_edited.sh
+++ code_/training/hpc_submit_training_Rh/submit_structural_numerical_ecfp_edited.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250205
+output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250317
 mkdir -p "$output_dir"
 
-regressors=("XGBR")
-targets=('Rh (1_1000 nm) (highest intensity)')
+regressors=("XGBR" "NGB" "RF")
+targets=('log Rg (nm)')
 models=("ECFP")
 radii=(3) 
 vectors=("count")
-poly_representations=('Dimer' 'RRU Dimer' 'Monomer' 'RRU Monomer' 'Trimer' 'RRU Trimer')
+poly_representations=('Trimer')
 
 for regressor in "${regressors[@]}"; do
   for target in "${targets[@]}"; do
@@ -20,12 +20,12 @@ for regressor in "${regressors[@]}"; do
 
 
 #BSUB -n 8
-#BSUB -W 72:05
+#BSUB -W 50
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
-#BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_20250205"  
-#BSUB -o "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${radius}_${vector}_20250205.out"
-#BSUB -e "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${radius}_${vector}_20250205.err"
+#BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_20250317"  
+#BSUB -o "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${radius}_${vector}_20250317.out"
+#BSUB -e "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${radius}_${vector}_20250317.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
@@ -35,11 +35,7 @@ python ../train_structure_numerical.py --target_features "${target}" \
                                       --radius "${radius}" \
                                       --vector "${vector}" \
                                       --oligomer_representation "${oligo_rep}" \
-                                      --numerical_feats 'Mn (g/mol)' 'PDI' 'Mw (g/mol)' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' \
-                                      --columns_to_impute "PDI" "Temperature SANS/SLS/DLS/SEC (K)" "Concentration (mg/ml)" \
-                                      --special_impute 'Mw (g/mol)' \
-                                      --imputer mean 
-
+                                      --numerical_feats 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' 
 
 
 EOT
@@ -51,4 +47,3 @@ EOT
   done
 done
 
-# 'Monomer' 'Dimer' 'Trimer' 'RRU Monomer' 'RRU Dimer'
