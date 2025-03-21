2a52711e395ab28d4016a9e1263faedecf39e5a2
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-03-21 09:46:00 -0500

    updated

--- code_/training/hpc_submit_training_Rh/submit_structure_only_ecfp.sh
+++ code_/training/hpc_submit_training_Rh/submit_structure_only_ecfp.sh
@@ -22,7 +22,7 @@ for regressor in "${regressors[@]}"; do
 #BSUB -W 30:01
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=8GB]"
-#BSUB -J "${fp}_${regressor}_${oligo_rep}_${target}_${radius}_${vector}_20250204"  
+#BSUB -J "${fp}_${regressor}_${oligo_rep}_${target}_${radius}_${vector}_20250321"  
 #BSUB -o "${output_dir}/${fp}_${regressor}_${oligo_rep}_${target}_${radius}_${vector}_20250321.out"
 #BSUB -e "${output_dir}/${fp}_${regressor}_${oligo_rep}_${target}_${radius}_${vector}_20250321.err"
 
