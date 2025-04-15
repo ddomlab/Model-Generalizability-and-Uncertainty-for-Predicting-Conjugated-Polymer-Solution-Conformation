2e75c18c0f65e0c3a2bc967abf1fa9ffdb732dd2
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-04-15 17:39:42 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_ood_mordred_learning_curve.sh
+++ code_/training/hpc_submit_training_Rh/submit_ood_mordred_learning_curve.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250402
+output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250415
 mkdir -p "$output_dir"
 
 regressors=("NGB" "XGBR")
 targets=('log Rg (nm)')
-models=("Mordred")
+models=("Mordred" "MACCS")
 poly_representations=('Trimer')
-group_out=('KM4 ECFP6_Count_512bit cluster'	'KM3 Mordred cluster'	'substructure cluster' 'KM5 polymer_solvent HSP and polysize cluster' 'KM4 polymer_solvent HSP cluster' 'KM4 Mordred_Polysize cluster') 
+group_out=('KM3 Mordred cluster' 'substructure cluster' 'KM4 polymer_solvent HSP and polysize cluster' 'KM5 polymer_solvent HSP and polysize cluster' 'KM4 polymer_solvent HSP cluster' 'KM4 Mordred_Polysize cluster') 
 
 
 for regressor in "${regressors[@]}"; do
@@ -18,13 +18,13 @@ for regressor in "${regressors[@]}"; do
 
 
 
-#BSUB -n 6
+#BSUB -n 4
 #BSUB -W 08:00
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
-#BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250402"  
-#BSUB -o "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250402.out"
-#BSUB -e "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250402.err"
+#BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250415"  
+#BSUB -o "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250415.out"
+#BSUB -e "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250415.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
@@ -32,7 +32,7 @@ python ../make_ood_learning_curve.py --target_features "${target}" \
                                       --representation "${fp}" \
                                       --regressor_type "${regressor}" \
                                       --oligomer_representation "${oligo_rep}" \
-                                      --numerical_feats 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' \
+                                      --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' \
                                       --clustering_method "${group}" \
 
 
