3f3b27822325ff9f818580c618f68e56ac89a904
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-05-02 09:38:07 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250417
+output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250502
 mkdir -p "$output_dir"
 
 # Correctly define models and numerical features
@@ -15,15 +15,15 @@ for target in "${target_to_assess[@]}"; do
 #BSUB -W 10:01
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=8GB]"
-#BSUB -J "numerical_${model}_with_feats_on_${target}_20250417"
-#BSUB -o "${output_dir}/numerical_${model}_polymer_${target}_20250417.out"
-#BSUB -e "${output_dir}/numerical_${model}_polymer_${target}_20250417.err"
+#BSUB -J "numerical_${model}_with_feats_on_${target}_20250502"
+#BSUB -o "${output_dir}/numerical_${model}_${target}_20250502.out"
+#BSUB -e "${output_dir}/numerical_${model}_${target}_20250502.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                   --regressor_type "${model}" \
-                                  --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH'
+                                  --numerical_feats  'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH'
 
 conda deactivate
 
@@ -31,3 +31,5 @@ EOT
         
     done
 done
+
+# 'Xn' 'Mw (g/mol)' 'PDI'
