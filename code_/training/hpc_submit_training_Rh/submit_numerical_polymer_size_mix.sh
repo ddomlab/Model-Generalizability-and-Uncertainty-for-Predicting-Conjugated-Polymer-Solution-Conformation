a7bc6aa91e423582e8a342dd2f945e618baf743f
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-05-15 21:26:57 -0500

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
@@ -12,7 +12,7 @@ for target in "${target_to_assess[@]}"; do
         bsub <<EOT
 
 #BSUB -n 4
-#BSUB -W 10:01
+#BSUB -W 2:01
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -J "numerical_${model}_with_feats_on_${target}_20250502"
