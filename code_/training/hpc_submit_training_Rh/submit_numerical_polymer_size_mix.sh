62ac6b8b86275cbeb6f23250472415ca46c7a563
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-05-28 15:42:08 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
@@ -4,7 +4,7 @@ mkdir -p "$output_dir"
 
 # Correctly define models and numerical features
 target_to_assess=('log Rg (nm)')
-models_to_run=("ElasticNet" "HGBR")
+models_to_run=("HGBR")
 
 
 for target in "${target_to_assess[@]}"; do
