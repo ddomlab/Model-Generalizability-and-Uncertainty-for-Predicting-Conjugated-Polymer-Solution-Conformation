e502f1578428e7ac034f561e1b1216624976c29a
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-05-29 18:12:06 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_ood_numerical_learning_curve.sh
+++ code_/training/hpc_submit_training_Rh/submit_ood_numerical_learning_curve.sh
@@ -16,7 +16,7 @@ for regressor in "${regressors[@]}"; do
 
 
 #BSUB -n 4
-#BSUB -W 03:00
+#BSUB -W 06:00
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
 #BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250529"  
@@ -27,7 +27,7 @@ source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../make_ood_learning_curve.py --target_features "${target}" \
                                       --regressor_type "${regressor}" \
-                                      --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' \
+                                      --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' "Dark/light" "Aging time (hour)" "To Aging Temperature (K)" "Sonication/Stirring/heating Temperature (K)" "Merged Stirring /sonication/heating time(min)" \
                                       --clustering_method "${group}" \
 
 
