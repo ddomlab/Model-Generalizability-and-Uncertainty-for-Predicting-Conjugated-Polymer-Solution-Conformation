d088bd3272dfe87de7b2a2ed371e404da7aa82e6
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-05-29 18:10:06 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_ood_mordred_learning_curve.sh
+++ code_/training/hpc_submit_training_Rh/submit_ood_mordred_learning_curve.sh
@@ -32,7 +32,7 @@ python ../make_ood_learning_curve.py --target_features "${target}" \
                                       --representation "${fp}" \
                                       --regressor_type "${regressor}" \
                                       --oligomer_representation "${oligo_rep}" \
-                                      --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' \
+                                      --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' "Dark/light" "Aging time (hour)" "To Aging Temperature (K)" "Sonication/Stirring/heating Temperature (K)" "Merged Stirring /sonication/heating time(min)" \
                                       --clustering_method "${group}" \
 
 
