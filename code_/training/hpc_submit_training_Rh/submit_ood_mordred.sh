1093d07213f5ed3b53304c1fbfa8ca3bf0b7d4ae
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-05-29 17:10:23 -0400

    updated for RF with aging stuff

--- code_/training/hpc_submit_training_Rh/submit_ood_mordred.sh
+++ code_/training/hpc_submit_training_Rh/submit_ood_mordred.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250515
+output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/hpc_20250529
 mkdir -p "$output_dir"
 
 regressors=("RF")
 targets=("log Rg (nm)")
-models=("MACCS" "Mordred")
+models=("Mordred")
 poly_representations=("Trimer")
-group_out=('substructure cluster')
+group_out=('substructure cluster' 'KM4 Mordred_Polysize cluster')
 # 'KM3 Mordred cluster' 'substructure cluster' 'KM4 polymer_solvent HSP and polysize cluster' 'KM5 polymer_solvent HSP and polysize cluster' 'KM4 polymer_solvent HSP cluster' 'KM4 Mordred_Polysize cluster'
 # 'KM3 Mordred cluster'
 # 'KM4 Mordred_Polysize cluster'
@@ -21,12 +21,12 @@ for regressor in "${regressors[@]}"; do
 
 
 #BSUB -n 6
-#BSUB -W 30:30
+#BSUB -W 45:30
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
-#BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250515"  
-#BSUB -o "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250515.out"
-#BSUB -e "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250515.err"
+#BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250529"  
+#BSUB -o "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250529.out"
+#BSUB -e "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250529.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
@@ -34,7 +34,7 @@ python ../make_ood_prediction.py --target_features "${target}" \
                                   --representation "${fp}" \
                                   --regressor_type "${regressor}" \
                                   --oligomer_representation "${oligo_rep}" \
-                                  --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' \
+                                  --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' "polymer dP" "polymer dD" "polymer dH" 'solvent dP' 'solvent dD' 'solvent dH' "Dark/light" "Aging time (hour)" "To Aging Temperature (K)" "Sonication/Stirring/heating Temperature (K)" "Merged Stirring /sonication/heating time(min)" \
                                   --clustering_method "${group}" \
 
 
