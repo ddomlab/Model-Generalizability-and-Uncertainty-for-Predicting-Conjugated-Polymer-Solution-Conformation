49bc42ebf43b8e8291358b2210701ca0b541b822
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-05-31 11:35:04 -0500

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
@@ -23,7 +23,7 @@ source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                   --regressor_type "${model}" \
-                                  --numerical_feats 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' 'solvent dP' 'solvent dD' 'solvent dH' "Dark/light" "Aging time (hour)" "To Aging Temperature (K)" "Sonication/Stirring/heating Temperature (K)" "Merged Stirring /sonication/heating time(min)" \
+                                  --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' \
 conda deactivate
 
 EOT
--- code_/training/hpc_submit_training_Rh/submit_ood_numerical_learning_curve.sh
+++ code_/training/hpc_submit_training_Rh/submit_ood_numerical_learning_curve.sh
@@ -27,7 +27,7 @@ source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../make_ood_learning_curve.py --target_features "${target}" \
                                       --regressor_type "${regressor}" \
-                                      --numerical_feats 'Concentration (mg/ml)' 'Temperature SANS/SLS/DLS/SEC (K)' 'solvent dP' 'solvent dD' 'solvent dH' "Dark/light" "Aging time (hour)" "To Aging Temperature (K)" "Sonication/Stirring/heating Temperature (K)" "Merged Stirring /sonication/heating time(min)" \
+                                      --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' \
                                       --clustering_method "${group}" \
 
 
