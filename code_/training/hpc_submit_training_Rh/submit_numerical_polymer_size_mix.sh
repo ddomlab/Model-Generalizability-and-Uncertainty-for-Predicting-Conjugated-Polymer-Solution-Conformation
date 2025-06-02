8f7913985c083aee0d4d3b3939b60852c2b8bfdc
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-06-02 15:03:59 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
@@ -4,15 +4,15 @@ mkdir -p "$output_dir"
 
 # Correctly define models and numerical features
 target_to_assess=('log Rg (nm)')
-models_to_run=("XGBR" "RF")
+models_to_run=("RF")
 
 
 for target in "${target_to_assess[@]}"; do
     for model in "${models_to_run[@]}"; do
         bsub <<EOT
 
-#BSUB -n 6
-#BSUB -W 10:01
+#BSUB -n 4
+#BSUB -W 7:01
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -J "numerical_${model}_with_feats_on_${target}_20250531"
@@ -23,7 +23,7 @@ source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                   --regressor_type "${model}" \
-                                  --numerical_feats 'Xn' 'Mw (g/mol)' 'PDI' "polymer dP" "polymer dD" "polymer dH"
+                                  --numerical_feats "Concentration (mg/ml)" "Temperature SANS/SLS/DLS/SEC (K)" "solvent dP" "solvent dD" "solvent dH"
 
 
 conda deactivate
