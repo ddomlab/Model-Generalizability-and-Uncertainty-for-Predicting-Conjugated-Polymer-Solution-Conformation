d935f087398dd209bef1aa9961a7f99288084315
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-06-09 13:45:36 -0400

    updated

--- code_/training/all_factories.py
+++ code_/training/all_factories.py
@@ -197,7 +197,7 @@ def get_regressor_search_space(algortihm:str, kernel:str=None) -> Dict :
         "regressor__regressor__max_depth": Integer(10, 10000, prior="log-uniform"),
         # "regressor__grow_policy": Categorical(["depthwise", "lossguide"]),
         "regressor__regressor__n_jobs": [-1],
-        "regressor__regressoreval__metric":["rmse"],
+        "regressor__regressor__eval_metric":["rmse"],
         "regressor__regressor__learning_rate": Real(1e-5, 1e-1, prior="log-uniform"),
         
     }
