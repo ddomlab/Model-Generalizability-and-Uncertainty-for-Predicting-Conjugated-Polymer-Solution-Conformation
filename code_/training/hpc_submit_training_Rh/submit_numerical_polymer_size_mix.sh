f76750774b2a06c4e10a4470492117d2594602e2
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-06-18 12:01:14 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
+++ code_/training/hpc_submit_training_Rh/submit_numerical_polymer_size_mix.sh
@@ -4,7 +4,7 @@ mkdir -p "$output_dir"
 
 # Correctly define models and numerical features
 target_to_assess=('log Rg (nm)')
-models_to_run=('DT' 'MLR' 'ElasticNet')
+models_to_run=('ElasticNet')
 
 
 for target in "${target_to_assess[@]}"; do
@@ -23,7 +23,7 @@ source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/pls-dataset-env
 python ../train_numerical_only.py --target_features "${target}" \
                                   --regressor_type "${model}" \
-                                  --numerical_feats "Dark/light" "Aging time (hour)" "To Aging Temperature (K)" "Sonication/Stirring/heating Temperature (K)" "Merged Stirring /sonication/heating time(min)"
+                                  --numerical_feats 'Mw (g/mol)' 'PDI' 
 
 conda deactivate
 
