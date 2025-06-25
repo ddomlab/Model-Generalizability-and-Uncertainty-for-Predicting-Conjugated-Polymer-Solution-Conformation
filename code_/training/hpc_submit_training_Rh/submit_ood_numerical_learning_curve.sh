fcdbfd677f7d16fd048caacceb7c22f60be7827a
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-06-25 10:12:51 -0400

    updated

--- code_/training/hpc_submit_training_Rh/submit_ood_numerical_learning_curve.sh
+++ code_/training/hpc_submit_training_Rh/submit_ood_numerical_learning_curve.sh
@@ -4,7 +4,7 @@ mkdir -p "$output_dir"
 
 regressors=("XGBR") 
 targets=('log Rg (nm)')
-group_out=('KM4 ECFP6_Count_512bit cluster' 'canonical_name') 
+group_out=('canonical_name') 
 # 'KM4 polymer_solvent HSP cluster' 'KM4 Mordred_Polysize cluster'
 # 'KM4 ECFP6_Count_512bit cluster' 'HBD3 MACCS cluster' 'KM3 Mordred cluster' 'substructure cluster' 'KM4 polymer_solvent HSP and polysize cluster' 'KM5 polymer_solvent HSP and polysize cluster' 'KM4 polymer_solvent HSP cluster' 'KM4 Mordred_Polysize cluster'
 
@@ -15,11 +15,10 @@ for regressor in "${regressors[@]}"; do
 
 
 
-#BSUB -n 10
+#BSUB -n 40
 #BSUB -W 15:30
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
-#BSUB -x
 #BSUB -J "${regressor}_${target}_${fp}_${oligo_rep}_${group}_20250624_lc"  
 #BSUB -o "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_numerical_20250624_lc.out"
 #BSUB -e "${output_dir}/${regressor}_${target}_${fp}_${oligo_rep}_${group}_numerical_20250624_lc.err"
